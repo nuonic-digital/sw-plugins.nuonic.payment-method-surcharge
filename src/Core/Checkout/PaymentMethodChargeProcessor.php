bce9981f08cfdcf86c7057c500fde4c22d4ffa7d
Alex Jank <alex@nuonic.de>
2025-02-13 15:17:57 +0100

    fix(cart): make orders work again

--- src/Core/Checkout/PaymentMethodChargeProcessor.php
+++ src/Core/Checkout/PaymentMethodChargeProcessor.php
@@ -4,30 +4,24 @@ declare(strict_types=1);
 
 namespace NuonicPaymentMethodSurcharge\Core\Checkout;
 
-use NuonicPaymentMethodSurcharge\Config\PluginConfigService;
 use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\CartBehavior;
 use Shopware\Core\Checkout\Cart\CartProcessorInterface;
 use Shopware\Core\Checkout\Cart\LineItem\CartDataCollection;
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
-use Shopware\Core\Checkout\Cart\LineItemFactoryRegistry;
 use Shopware\Core\Checkout\Cart\Price\PercentagePriceCalculator;
 use Shopware\Core\Checkout\Cart\Price\Struct\PercentagePriceDefinition;
 use Shopware\Core\Content\Product\Cart\ProductCartProcessor;
-use Shopware\Core\Framework\Util\FloatComparator;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Core\System\SystemConfig\Exception\InvalidSettingValueException;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 readonly class PaymentMethodChargeProcessor implements CartProcessorInterface
 {
     public const LINE_ITEM_REFERENCE = 'NuonicPaymentMethodSurcharge';
+    public const LINE_ITEM_TYPE = 'nuonic-payment-method-surcharge';
 
     public function __construct(
         private PercentagePriceCalculator $calculator,
-        private PluginConfigService $pluginConfigService,
-        private LineItemFactoryRegistry $lineItemFactory,
-        private TranslatorInterface $translator,
     ) {
     }
 
@@ -38,55 +32,39 @@ readonly class PaymentMethodChargeProcessor implements CartProcessorInterface
         SalesChannelContext $context,
         CartBehavior $behavior,
     ): void {
-        $paymentMethodIds = $this->pluginConfigService->get('paymentMethodIds', $context->getSalesChannelId());
-        if (!is_array($paymentMethodIds) || !in_array($context->getPaymentMethod()->getId(), $paymentMethodIds, true)) {
-            // this will also return here, if paymentMethodId is null :)
+        $products = $original->getLineItems()->filterType(LineItem::PRODUCT_LINE_ITEM_TYPE);
+        if (0 === count($products)) {
             return;
         }
 
-        // if no percentage is defined -> nothing to do
-        try {
-            $chargePercentage = $this->pluginConfigService->getFloat('chargePercentage', $context->getSalesChannelId());
-        } catch (InvalidSettingValueException $e) {
+        if (!$data->has(__CLASS__)) {
             return;
         }
 
-        if (FloatComparator::equals($chargePercentage, 0.0)) {
-            return;
-        }
+        $lineItemData = $data->get(__CLASS__);
 
-        $context->setPermissions([...$context->getPermissions(), ProductCartProcessor::ALLOW_PRODUCT_PRICE_OVERWRITES => true]);
+        $lineItem = new LineItem(
+            $original->getLineItems()->filterType(self::LINE_ITEM_TYPE)->first()?->getId() ?? Uuid::randomHex(),
+            self::LINE_ITEM_TYPE,
+            self::LINE_ITEM_REFERENCE,
+            1
+        );
 
-        $lineItem = $this->lineItemFactory->create([
-            'type' => LineItem::CUSTOM_LINE_ITEM_TYPE,
-            'referencedId' => self::LINE_ITEM_REFERENCE,
-            'quantity' => 1,
-            'payload' => [],
-        ], $context);
-
-        $lineItemLabel = trim($this->pluginConfigService->getString(
-            'chargeLineItemLabel',
-            $context->getSalesChannelId()
-        ));
-
-        if ('' === $lineItemLabel) {
-            $lineItemLabel = $this->translator->trans('nuonicPaymentMethodSurcharge.defaultLineItemLabel');
-        }
-
-        $lineItem->setLabel($lineItemLabel);
+        $lineItem->setLabel($lineItemData['chargeLineItemLabel']);
         $lineItem->setGood(false);
         $lineItem->setStackable(false);
         $lineItem->setRemovable(false);
 
-        $definition = new PercentagePriceDefinition($chargePercentage);
+        $definition = new PercentagePriceDefinition($lineItemData['chargePercentage']);
 
-        $lineItem->setPriceDefinition($definition);
-
-        $lineItem->setPrice($this->calculator->calculate(
+        $price = $this->calculator->calculate(
             $definition->getPercentage(),
             $toCalculate->getLineItems()->getPrices(),
             $context
-        ));
+        );
+
+        $lineItem->setPrice($price);
+        $lineItem->setPriceDefinition($definition);
 
         $originalLineItem = $original->getLineItems()
             ->filter(fn (LineItem $lineItem) => self::LINE_ITEM_REFERENCE === $lineItem->getReferencedId())
